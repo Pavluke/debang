08521e9b4a5f8a1fee57bc95eab292e273975a5c
Pavluke <dpvlnk@ya.ru>
2025-09-22 10:04:18 +0300

    2

--- lib/debang.dart
+++ lib/debang.dart
@@ -57,3 +57,22 @@ abstract class Debang<T> implements Exception {
   /// Default options for customizing the output of a [Debang] exception.
   static DebangOptions options = DebangOptions();
 }
+// Extension for convenient null checking.
+extension DebangExtension<T> on T? {
+  /// Returns the non-null value or throws a [Debang] exception.
+  ///
+  /// If the value is null, a [Debang] exception is thrown with the provided [assertion].
+  /// The [assertion] is a required description of the guarantee or assumption made
+  /// by the developer about why this value must not be null.
+  ///
+  /// This makes null-safety violations explicit and self-documented.
+  ///
+  /// Example:
+  /// ```dart
+  /// String? value = Random().nextBool()? 'Hello!': null;
+  /// String newValue = value.debang(
+  ///   'newValue is guaranteed to be non-null because it is initialized earlier'
+  /// );
+  /// ```
+  T debang(String assertion, [StackTrace? st]) => this ?? (throw Debang<T>(assertion, st ?? StackTrace.current));
+}
